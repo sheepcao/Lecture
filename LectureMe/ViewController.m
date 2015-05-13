5d5e8a27cc32fbdd2866db83414fdf4efc8f4e0f
Eric Cao <ericcao@ML00070057.local>
2015-05-13 10:12:19 +0800

    Initial Commit

---
+++ LectureMe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LectureMe
+//
+//  Created by Eric Cao on 5/13/15.
+//  Copyright (c) 2015 sheepcao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LectureMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LectureMe
+//
+//  Created by Eric Cao on 5/13/15.
+//  Copyright (c) 2015 sheepcao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
